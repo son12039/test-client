3cac0a2ad21655cd330b7b43e28f3c776e9fba3b
son12039 <sonson12039@gmail.com>
2024-09-27 16:43:58 +0900

    0927

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8080");
+const socket = io("https://test-server-zgr2.onrender.com");
 
 function App() {
   const [like, setLike] = useState(0);
